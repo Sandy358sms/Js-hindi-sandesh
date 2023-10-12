a008c9a0a4191f100d1c155cef1af98a97cca85b
Sandy358sms <126079331+Sandy358sms@users.noreply.github.com>
2023-10-12 18:40:44 +0000

    conversionOperations

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -17,9 +17,31 @@ let score = "22"
 
 let isCorrect = 1;
 let boolisCorrect = Boolean(isCorrect)
-console.log(boolisCorrect);
+// console.log(boolisCorrect);
 
 // 1 => true
 // 0 => false
 // "" => false
 // "san" => ture
+// *************** Operations *************
+let value =10
+let negvalue = - value
+
+// console.log(negvalue);
+// console.log(10+10);
+// console.log(10-10);
+// console.log(10*10);
+// console.log(10/5);
+// console.log(5%10);
+
+// let str1 = "sandesh"
+// let str2 = "wankhede"
+// let str3 = str1 + str2
+// console.log(str3);
+
+console.log(1 + "2"); //=>12
+console.log("1"+2+2); //=>122
+
+console.log(1+2+"2"+"3");
+
+
