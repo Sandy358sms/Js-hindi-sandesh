d1db099a886d0dc510b3a38260ad2420164f313b
Sandy358sms <126079331+Sandy358sms@users.noreply.github.com>
2023-10-12 18:20:15 +0000

    03_conversionOperations

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "22"
+// console.log(typeof score);
+
+// conversion of string into number
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// null => 0
+// "33" => 33
+// "33abc" => NaN
+// true => 1 
+// false => 0
+// undefined => NaN
+
+
+let isCorrect = 1;
+let boolisCorrect = Boolean(isCorrect)
+console.log(boolisCorrect);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "san" => ture
