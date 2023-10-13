993ba744590f517377e0dcdbf52ee7b0b28af1cd
Sandy358sms <126079331+Sandy358sms@users.noreply.github.com>
2023-10-13 11:40:44 +0000

    container config done

---
+++ 01_Basics/array.js
@@ -0,0 +1,15 @@
+const myArr1 = [1,2,3,4,5]
+// console.log(myArr1);
+const myHeros = ["Baalveer","juniorG"]
+// console.log(myHeros);
+const myArr2 = new Array(10,20,30,40)
+// console.log(myArr2);
+
+// Array methods
+// myArr1.push(6)
+// console.log(myArr1);
+// myArr1.pop();
+// console.log(myArr1);
+
+// console.log(myArr1.includes(4));
+// console.log(myArr1.indexOf(4));
