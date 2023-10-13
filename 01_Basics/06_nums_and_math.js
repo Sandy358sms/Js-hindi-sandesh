d0abecfe0dc6411638bb371960930a9597ab1bdc
Sandy358sms <126079331+Sandy358sms@users.noreply.github.com>
2023-10-13 11:16:21 +0000

    nums_and_math

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,20 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+// const num = 12.5565
+// // prcision round of krun denar if dec chya nntr >5 asel tr
+// console.log(num.toPrecision(2));
+
+// const hundreds = 100000000
+// // toLocaleString('en-IN)
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ******** Maths *******
+console.log(Math);
+console.log(Math.abs(-19));
+console.log(Math.round(10.5));
+
