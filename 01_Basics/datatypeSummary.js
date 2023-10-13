4005bc42e6aec74e2ef8411f55cfdd429d629651
Sandy358sms <126079331+Sandy358sms@users.noreply.github.com>
2023-10-13 05:53:01 +0000

    datatypeSummary

---
+++ 01_Basics/datatypeSummary.js
@@ -0,0 +1,23 @@
+// Primitive datatypes:
+// 7 types : String,Number,null,undefined,Symbol,BigInt,Boolean
+
+// reference (Non primitive) datatypes:
+// array,object,function
+
+// const heros = [ironman,batman,hulk];
+// console.log(heros);
+
+let myObj = {
+    name: "sandy",
+    age: 23,
+    prn: 21510096,
+}
+
+let myfun = function (){
+    console.log("hello");
+}
+
+console.log(myObj);
+
+console.log(myfun);
+// console.log("sandesh wankhede");
