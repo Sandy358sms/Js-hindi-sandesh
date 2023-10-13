bfe7f55c2e72b75a4b72f9dcbc0d3f9f1dca0b77
Sandy358sms <126079331+Sandy358sms@users.noreply.github.com>
2023-10-13 09:08:02 +0000

    datatypeSummary

--- 01_Basics/datatypeSummary.js
+++ 01_Basics/datatypeSummary.js
@@ -17,7 +17,34 @@ let myfun = function (){
     console.log("hello");
 }
 
-console.log(myObj);
+// console.log(myObj);
 
-console.log(myfun);
+// console.log(myfun);
 // console.log("sandesh wankhede");
+
+// **************************
+// stack(primitive) and 
+// heap(reference)
+let myname = "sandesh"
+let anothername = "pappu"
+
+anothername = "sandy"
+
+console.log(myname);
+// console.log(anothername);
+// console.log(anothername);
+
+let userOne = {
+    name:"sandesh",
+    prn: 21510096
+}
+
+let userTwo = {
+    name:"pratik",
+    prn: 21510094
+}
+console.log(userOne);
+console.log(userTwo);
+userTwo = userOne;
+console.log(userOne.name);
+console.log(userTwo.name);
